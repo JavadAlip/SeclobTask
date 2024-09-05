7d5448b4e4a76a72443e57a9133a28832f39110e
Javad Ali P <javadalipp911@gmail.com>
2024-09-05 16:41:55 +0530

    Footer setup done.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Logo from '../assets/aloha.png';
 const Footer = () => {
   return (
-    <div className='bg-primaryGreen p-4'>Footer</div>
+    <div className='bg-primaryGreen p-4'>
+         <div>
+        <img src={Logo} alt="" style={{ width: '50px', height: '25px', cursor: 'pointer' }} />
+        </div>
+    </div>
   )
 }
 
